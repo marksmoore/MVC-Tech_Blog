53f9b8cf1671e07bc6ebca183c230dac7b20512f
Marco <msmo1960@gmail.com>
2021-07-07 15:39:44 -0500

    commit

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,44 +4,44 @@ const { User, Post } = require('../models');
 const userdata = [
   {
     username: 'alesmonde0',
+    email: 'martin@gmail.com',
     password: 'password123'
   },
   {
     username: 'jwilloughway1',
+    email: 'matt@gmail.com',
     password: 'password123'
   },
   {
     username: 'iboddam2',
+    email: 'don@gmail.com',
     password: 'password123'
   },
   {
     username: 'dstanmer3',
+    email: 'joy@gmail.com',
     password: 'password123'
   },
   {
     username: 'djiri4',
+    email: 'kenneth@gmail.com',
     password: 'password123'
   },
   {
     username: 'msprague5',
+    email: 'mark@gmail.com',
     password: 'password123'
   },
   {
     username: 'mpergens6',
+    email: 'mike@gmail.com',
     password: 'password123'
   },
   {
     username: 'tpenniell7',
+    email: 'kim@gmail.com',
     password: 'password123'
   },
-  {
-    username: 'msabbins8',
-    password: 'password123'
-  },
-  {
-    username: 'jmacarthur9',
-    password: 'password123'
-  }
 ];
 
 const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
