9d18a28398afad816a5dfa042b888a784fc6d38d
Marco <msmo1960@gmail.com>
2021-07-08 13:53:02 -0500

    commit

--- models/index.js
+++ models/index.js
@@ -1,33 +1,30 @@
-const Post = require('./Post');
 const User = require('./User');
+const Post = require('./Post');
 const Comment = require('./Comment');
 
+//create associations
 User.hasMany(Post, {
-  foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL',
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL',
+    foreignKey: 'user_id',
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL',
-});
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-});
-
+    foreignKey: 'user_id'
+  });
+  
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL',
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Comment };
+module.exports = {User, Post, Comment};
